f41748086e89acb2395245c29906e466399ce454
lucassaro12 <lucas.saro.127714@a.fecaf.com.br>
2025-11-10 22:51:33 -0300

    test: Adiciona dependência e teste de contexto

---
+++ src/test/java/br/ApiMotosApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.fecaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMotosApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Este teste simples "passa" se o seu
+        // projeto Spring (sua API) conseguir ligar sem erros.
+    }
+}
